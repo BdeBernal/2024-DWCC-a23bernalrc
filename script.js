fda9a090668cecffa7f872ba9b70d53c02a6c648
Bernal <=>
2024-09-27 10:23:21 +0200

    Poco de js

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log(10);
+
+const i = 20;
+
+console.log(i);
+
+console.warn("Paco");
+
+console.error("Pakito");
+
+console.table({ Nombre: "Paco", Edad: 12 });
+// Cuando imprimes un objeto no lo imprime en el momento concreto, imprime su valor final
