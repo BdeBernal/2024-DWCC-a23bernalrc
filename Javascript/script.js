52882b2df292526b36dad897bd45006403b519a4
Bernal <=>
2024-10-07 16:51:01 +0200

    Ejercicios Inicio sencillos

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,34 +1,102 @@
-"use strict";
+'use strict';
 
-console.log(10);
+//Exercise 1
+function diaLaborable() {
+    let dia = "Lunes";
 
-const i = 20;
+    if (dia == "Domingo" || dia == "Sabado") {
+        console.log(dia + " no es laborable");
+    } else {
+        console.log(dia + " laborable");
+    }
+}
 
-console.log(i);
+diaLaborable();
 
-console.warn("Paco");
+//Exercise 2
+function numeroMayor() {
+    let numero1 = 4;
+    let numero2 = 3;
+    let numero3 = 2;
 
-console.error("Pakito");
+    if (numero1 > numero2) {
+        if (numero1 > numero3) {
+            console.log(numero1 + " es el mayor de los tres números")
+        } else {
+            console.log(numero3 + " es el mayor de los tres números")
+        }
+    } else {
+        if (numero2 > numero3) {
+            console.log(numero2 + " es el mayor de los tres números")
+        } else {
+            console.log(numero3 + " es el mayor de los tres números")
+        }
+    }
+}
 
-console.table({ Nombre: "Paco", Edad: 12 });
-// Cuando imprimes un objeto no lo imprime en el momento concreto, imprime su valor final
+numeroMayor();
 
-//Declarar variables
-var x = 0; //Dentro de función o global se pueden declarar misma variables mismo nombre, hoisting
+//Exercise 3
+function numerosPares() {
+    for (let x = 0; x <= 30; x++){
+        if (x % 2 == 0) {
+            console.log(x + " es par")
+        }
+    }
+}
 
-let y = 0; // No se puede declarar varibles mismo nombre salvo ámbito de bloque
+numerosPares();
 
-const z = 0; //Constante para todo el programa, no se puede cambiar valor
+//Exercise 4
+function potencias() {
+    for (let x = 0; x <= 20; x++){
+        console.log("2 elevado a " + x + " = " + 2 ** x);
+    }
+}
 
-//Tipos de datos (boolean, number, NaN, bigInt, string, undefined, null, symbol, object)
+potencias();
 
-let a = {
-    name:"John",
-    age:22
-    };
-    let b = a;
-    b.name = "Paco";
-    console.log('a => ', a);
-    console.log('b => ', b);
+//Exercise 5
+function calcularFactorial() {
+    let total = 1;
+    let numero = 5;
     
-//Nuevo objeto mismo puntero
+    for (numero; numero > 0; numero--){
+        total *= numero;
+    }
+    console.log("El factorial de 5 es = " + total);
+}
+
+calcularFactorial();
+
+//Exercise 6
+function calcularImc() {
+    let pesoPersona1 = 70;
+    let alturaPersona1 = 1.70;
+
+    let pesoPersona2 = 90;
+    let alturaPersona2 = 1.90;
+
+    function matematico(peso, altura) {
+        let resultado = peso / altura ** 2;
+        return resultado;
+    }
+
+    let resultadoPersona1 = matematico(pesoPersona1, alturaPersona1).toFixed(2);
+    let resultadoPersona2 = matematico(pesoPersona2, alturaPersona2).toFixed(2);
+    
+    if (resultadoPersona1 > resultadoPersona2) {
+        console.log("O IMC " + resultadoPersona1 + " da primeira persoa é maior que o da segunda persoa " + resultadoPersona2);
+    } else {
+        console.log("O IMC " + resultadoPersona2 + " da segunda persoa é maior que o da primeira persoa " + resultadoPersona1);
+    }
+}
+
+calcularImc();
+
+//Exercise function
+function calcularDescuento(precio, porcentaje) {
+    return precio = precio - (precio * (porcentaje / 100));
+}
+
+console.log(calcularDescuento(8, 10));
