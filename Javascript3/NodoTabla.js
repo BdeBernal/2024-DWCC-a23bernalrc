a9e549ee31f31f65234dc7d7b09a38c58c1c1d06
Bernal <b3rn4lrc@gmail.com>
2024-11-11 18:43:13 +0100

    Ejercicios done

--- Javascript3/NodoTabla.js
+++ Javascript3/NodoTabla.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// Acceder ao nodo div
+console.log(document.querySelector('div'));
+
+// Acceder ao nodo ul
+console.log(document.querySelector('ul'));
+
+// Acceder ao segundo nodo li
+console.log(document.querySelector('ul li:nth-child(2)'));
+
+// ¿É certo que elemento.lastChild.nextSibling é sempre null? -> No
+// ¿É certo que elemento.children[0].previousSibling é sempre null? -> No
+
+// Pintar as celas diagonais dunha táboa de vermello
+const table = document.querySelector('table');
+for (let i = 0; i < table.rows.length; i++) {
+    table.rows[i].cells[i].style.backgroundColor = 'red';
+}
