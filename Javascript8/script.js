03a31d12e1b3de53c73c88616109486f96677a60
Bernal <=>
2025-01-09 13:33:02 +0100

    Nuevo año

---
+++ Javascript8/script.js
@@ -0,0 +1,67 @@
+'use strict';
+
+//1. Implementa unha función escribirNumeros(desde, ata) que xere un número cada
+//segundo, comezando en “desde” e rematando en “ata”. Fai dúas variantes da
+//función: unha usando setInterval e outra usando setTimeout.
+
+function escribirNumeros(desde, ata) {
+    const intervalID = setInterval(() => {
+        console.log(desde);
+        if (desde === ata) {
+            clearInterval(intervalID);
+        }
+        desde++;
+    }, 1000
+    ); 
+}
+
+escribirNumeros(1, 10);
+
+function escribirMasNumeros(desde, ata) {
+    if (desde <= ata) {
+        setTimeout(() => {
+            console.log(desde);
+            escribirMasNumeros(desde + 1, ata);
+        }, 1000);
+    }
+};
+
+escribirMasNumeros(1, 10);
+
+//2. Crea unha páxina web con un título <h1> e dous botóns. Un dos botóns debe iniciar
+//un intervalo que cambie alternativamente cada segundo a cor do título <h1>
+//alternando entre dúas cores, as que escollas. O outro botón debe deter o intervalo e
+//parar o cambio de cores.
+
+function numero(max) {
+    return Math.floor(Math.random() * max);
+}
+
+let idInterval;
+
+const color = document.getElementById('cambiarColor');
+const h1 = document.getElementById('h1');
+const parar = document.getElementById('parar');
+
+color.addEventListener('click', () => {
+    idInterval = setInterval(() => {
+        if (h1.style.color == 'black') {
+            h1.style.color = 'red';
+        } else {
+            h1.style.color = 'black';
+        }
+    }, 1000)
+});
+
+parar.addEventListener('click', () => {
+    clearInterval(idInterval);
+});
+
+//3. Busca unha imaxe grande en internet para probar que tarda tempo en descargarse.
+//Carga esa imaxe remota dende JavaScript. Cando a imaxe estea cargada, mostra
+//unha mensaxe en consola.
+
+let img = document.createElement('img');
+document.body.appendChild(img)
+img.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1xI1LqxCGglcQZVVo_XEg-9r1zZ4tZfDE3A&s');
+console.log('imagen cargada');
